a5da6d5edbab659a81e46b3bfb589684c06fb919
nayeong lee <lny021102@gmail.com>
2024-02-06 03:41:34 +0900

    [ADD] modal animation 추가

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import StyledImg from '../../Components/Container/StyledImg';
+import { keyframes } from 'styled-components';
 
 const ContainerCenter = styled.div`
   display: flex;
@@ -23,7 +25,7 @@ const PageContainer = styled(ContainerCenter)`
 
 const Next = styled.button`
 width: 200px;
-height: 50px;
+height: 80px;
 border: none;
 background-color: #5EB371;
 border-radius: 20px;
@@ -38,25 +40,74 @@ justify-content: center;
 gap: 15px;
 
   position: absolute;
-  top: 67%;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 1;
 `;
 
+const growAnimation = keyframes`
+  from {
+    width: 20%;
+    height: 20%;
+  }
+  to {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+
+  & > div {
+    animation: ${growAnimation} 0.5s ease-out forwards;
+  }
+`;
+
+const ModalContent = styled.div`
+  width: 20%; /* 초기 크기 설정 */
+  height: 20%;
+  transition: width 0.5s, height 0.5s; /* 부드러운 전환 효과 */
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default function Component() {
 
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   const handleClick = () => {
-    navigate('/letterpage');
+    setShowModal(true);
+    setTimeout(() => {
+      navigate('/letterpage'); // 3초 후 페이지 이동
+    }, 3000);
   };
 
   return (
     <ContainerCenter>
       <PageContainer>
-        <StyledImg src={'LetterBag.png'} />
+        <StyledImg src={'icon_letter_close.png'} width='70%' height='50vh' />
         <Next onClick={handleClick}>스티커 뜯기</Next>
+        {showModal && (
+          <Modal>
+            <ModalContent>
+              <StyledImg src={'icon_letter_open.png'} />
+            </ModalContent>
+          </Modal>
+        )}
       </PageContainer>
     </ContainerCenter>
   );
